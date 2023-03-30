95de770e61dcf23e134070893c68b51f2218a42e
tmhafidh <teukumhr@gmail.com>
2023-03-31 00:27:37 +0700

    update tipe data number

---
+++ TipeDataNumber.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Decimal : ";
+var_dump(12345);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b111111);
+
+// echo "Underscore in number : ";
+// var_dump(1_341_434_43);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation Plus (1.2 x 1000) : ";
+var_dump(1.2e3);
+
+echo "Floating Point dengan E Notation Minus (7 x 0.001) : ";
+var_dump(7e-3);
+
+echo "Underscore di Floating Point: ";
+var_dump(121.211);
+
+echo "Integer Overflow 65 bit: ";
+var_dump(121321324234323423432432);
